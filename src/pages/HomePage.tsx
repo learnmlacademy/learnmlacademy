4595b1ba4b23ff1e02880e6209765cdd504747b5
learnmlacademy <machinelearningacademy83@gmail.com>
2026-07-17 21:18:55 +0530

    Add files via upload

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -241,7 +241,7 @@ export function HomePage() {
 
           <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {curriculum.map((cat, i) => (
-              <ModuleCard key={cat.id} cat={cat} index={i} />
+              <div key={cat.id}><ModuleCard cat={cat} index={i} /></div>
             ))}
           </div>
         </div>
