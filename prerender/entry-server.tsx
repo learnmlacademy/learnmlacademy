67676bd03307bf5bed5d195b9ff75b9a209b90d4
learnmlacademy <machinelearningacademy83@gmail.com>
2026-09-22 21:16:10 +0530

    Use React Router server-safe StaticRouter export

--- prerender/entry-server.tsx
+++ prerender/entry-server.tsx
@@ -1,5 +1,5 @@
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
+import { StaticRouter } from 'react-router';
 
 export function render(url: string): string {
   return renderToString(
