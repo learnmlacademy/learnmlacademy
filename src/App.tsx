a63fbcc46669ea745918799fc0c4215a26fb4e55
learnmlacademy <machinelearningacademy83@gmail.com>
2026-09-22 21:03:38 +0530

    Expose app routes for static prerendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppLayout } from './components/layout/AppLayout';
 import { HomePage } from './pages/HomePage';
 import { TopicPage } from './pages/TopicPage';
@@ -12,23 +12,29 @@ import { BlogPage } from './pages/BlogPage';
 import { BlogPostPage } from './pages/BlogPostPage';
 import { CheatsheetPage } from './pages/CheatsheetPage';
 
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="learn/:topicId" element={<TopicPage />} />
+        <Route path="curriculum" element={<CurriculumPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="blog" element={<BlogPage />} />
+        <Route path="blog/:slug" element={<BlogPostPage />} />
+        <Route path="cheatsheet" element={<CheatsheetPage />} />
+        <Route path="privacy" element={<PrivacyPolicyPage />} />
+        <Route path="terms" element={<TermsOfServicePage />} />
+        <Route path="disclaimer" element={<DisclaimerPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="learn/:topicId" element={<TopicPage />} />
-          <Route path="curriculum" element={<CurriculumPage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="blog" element={<BlogPage />} />
-          <Route path="blog/:slug" element={<BlogPostPage />} />
-          <Route path="cheatsheet" element={<CheatsheetPage />} />
-          <Route path="privacy" element={<PrivacyPolicyPage />} />
-          <Route path="terms" element={<TermsOfServicePage />} />
-          <Route path="disclaimer" element={<DisclaimerPage />} />
-        </Route>
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
